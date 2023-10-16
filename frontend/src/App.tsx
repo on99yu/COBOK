9f6fa113519ef894049ed1cae08b1c3917b7f9ea
on99yu <dgjjanggu@naver.com>
2023-10-17 00:08:11 +0900

    [Style] import 정리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,11 @@ import MainNavbar from './components/Main.Navbar'
 import {Routes, Route} from 'react-router-dom';
 
 import GlobalStyle  from './theme';
-import { Main } from './pages/Main.page';
+import Main from './pages/Main.page';
 import { AutoTrading } from './pages/AutoTrading.page';
-const App : React.FC= ()=> {
+
+
+const App:React.FC = ()=> {
   return (
     <>
       <GlobalStyle/>
