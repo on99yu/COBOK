bc2bea69c0397fa20f2a1d6626c68263d2d18ba2
on99yu <dgjjanggu@naver.com>
2023-10-16 11:59:10 +0900

    [Modify] 컨테니어 구조 변경

--- frontend/src/pages/Main.page.tsx
+++ frontend/src/pages/Main.page.tsx
@@ -1,22 +1,51 @@
 import React from 'react'
 import styled from "styled-components"
 
+import { CoinInfo } from '../components/CoinInfo.component';
+import { SelectTickerBox } from '../components/SelectTickerBox.component';
 export function Main(){
     return (
-    <CoinInfoContainer>
-        CoininfoContainer
-    </CoinInfoContainer>
+    <MainContainer>
+        <CoinInfoContainer>
+            <CoinPriceContainer>
+                <CoinInfo/>
+            </CoinPriceContainer>
+        </CoinInfoContainer>
+        <SelectTickerContainer>
+            <SelectTickerBox/>
+        </SelectTickerContainer>
+    </MainContainer>
     );
 };
+const MainContainer = styled.div`
 
-const CoinInfoContainer = styled.div`
-    width:80%;
-    height: 600px;
-    
     margin-top: 5%;
     margin-left: 10%;
     margin-right: 10%;
     margin-bottom: 15%;
 
-    border: 2px solid ${props=>props.theme.Main5}
+    display: flex;
+    flex-direction: row;
+`
+
+const CoinInfoContainer = styled.div`
+
+    min-width: 700px;
+    width:80%;
+    height: 180px;
+
+    border: 2px solid ${props=>props.theme.Main5};
+    border-radius: 16px;
+`
+const CoinPriceContainer = styled.div`
+    width: 40%;
+    height:100%;
+`
+const SelectTickerContainer = styled.div`
+    width:20%;
+    height: auto;
+    margin-left: 100px;
+
+    border: 2px solid ${props=>props.theme.Yellow};
+    /* border-radius: 16px; */
 `
